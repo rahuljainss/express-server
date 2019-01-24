91bec4ee2cff8fd3fbf27d0700a995045d4cdf7a
rahuljainss <rahul.jain@successive.tech>
2019-01-24 13:03:51 +0530

    docs:Advantages of typescript

--- extraTs/constants.ts
+++ extraTs/constants.ts
@@ -1,20 +1,20 @@
 import { IPermissions } from "./interface";
-export const headTrainer = "head-trainer";
-export const trainee = "trainee";
-export const trainer = "trainer";
-export const getUsers = "getUsers";
-export const getUsers1 = "getUsers1";
-export const permission: IPermissions = {
-  getUsers: {
-    all: [headTrainer],
-    read: [trainee, trainer],
-    write: [trainer],
+export const HEADTRAINER: string = "head-trainer";
+export const TRAINEE: string = "trainee";
+export const TRAINER: string = "trainer";
+export const GETUSERS: string = "getUsers";
+export const GETUSERS1: string = "getUsers1";
+export const PERMISSION: IPermissions = {
+  GETUSERS: {
+    all: [HEADTRAINER],
+    read: [TRAINEE, TRAINER],
+    write: [TRAINER],
     delete: []
   },
-  getUsers1: {
-    all: [headTrainer],
-    read: [trainee, trainer],
-    write: [trainer],
+  GETUSERS1: {
+    all: [HEADTRAINER],
+    read: [TRAINEE, TRAINER],
+    write: [TRAINER],
     delete: []
   }
 };
